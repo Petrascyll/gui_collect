cd10627041333d9235ee894ecd0c939be31fe95f
petrascyll <124032137+Petrascyll@users.noreply.github.com>
2025-06-07 23:15:56 -0400

    Try running python in launch bat if py is missing

--- launch.bat
+++ launch.bat
@@ -1,37 +1,37 @@
 @echo off
+setlocal enabledelayedexpansion
 
-@REM Behold, possibly the laziest but simplest approach to
-@REM learn if the minimum required python version is available
-@REM and to run the collect script using it
+py --version >nul 2>&1
+if %errorlevel% == 0 (
+    for /f "tokens=2 delims= " %%A in ('py --version 2^>^&1') do set "ver=%%A"
+    call :run_script py
+    goto end
+)
 
-@REM Try every single python version starting from 3.14 to 3.9
-set "valid-python-version-list=3.14 3.13 3.12 3.11 3.10 3.9"
-set "python-version=0"
+python --version >nul 2>&1
+if %errorlevel% == 0 (
+    for /f "tokens=2 delims= " %%A in ('python --version 2^>^&1') do set "ver=%%A"
+    call :run_script python
+    goto end
+)
 
-@REM https://stackoverflow.com/a/40192472
-@REM    "Whenever Windows command interpreter encounters
-@REM    ( being interpreted as begin of a command block,
-@REM    it parses the entire command block up to matching ) 
-@REM    marking end of the command block and replaces all
-@REM    %variable%  by current value of the variable
-@REM    ... Delayed variable expansion is needed for variables set or 
-@REM    modified and referenced within same command block..."
-setlocal EnableDelayedExpansion
-
-(for %%v in (%valid-python-version-list%) do (
-    py -%%v --version >nul 2>&1
-    if !errorlevel! == 0 (
-        set "python-version=%%v"
-        goto run-script
-    )
-))
-
-echo You must have at least Python 3.9 installed! 
+echo Python not found!
 pause
 goto end
 
-:run-script
-py -%python-version% --version
-py -%python-version% ./collect.py
+:run_script
+for /f "tokens=1,2 delims=." %%A in ("%ver%") do (
+    set "major=%%A"
+    set "minor=%%B"
+)
+
+if !major! EQU 3 if !minor! GTR 8 (
+    %1 --version
+    %1 ./collect.py
+    goto :eof
+) else (
+    echo You must have at least Python 3.9 installed! 
+    pause
+)
 
 :end
